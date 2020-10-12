b7b4076aa55f48a4ab29a78cc8c77816fdcf320d
oshando <oshando@campus.uni-paderborn.de>
2020-10-12 09:45:02 +0200

    Refactor ML-Plan integration

--- swan_core/src/main/java/de/fraunhofer/iem/swan/model/MLPlanExecutor.java
+++ swan_core/src/main/java/de/fraunhofer/iem/swan/model/MLPlanExecutor.java
@@ -1,23 +1,31 @@
 package de.fraunhofer.iem.swan.model;
 
-import ai.libs.jaicore.logging.LoggerUtil;
-import ai.libs.jaicore.ml.weka.dataset.WekaInstances;
+import ai.libs.jaicore.ml.classification.loss.dataset.EClassificationPerformanceMeasure;
+import ai.libs.jaicore.ml.core.dataset.serialization.ArffDatasetAdapter;
+import ai.libs.jaicore.ml.core.evaluation.evaluator.SupervisedLearnerExecutor;
+import ai.libs.jaicore.ml.core.filter.SplitterUtil;
+import ai.libs.jaicore.ml.weka.classification.learner.IWekaClassifier;
+import ai.libs.mlplan.core.MLPlan;
 import ai.libs.mlplan.multiclass.wekamlplan.MLPlanWekaBuilder;
-import de.fraunhofer.iem.swan.util.Util;
+import org.api4.java.ai.ml.classification.singlelabel.evaluation.ISingleLabelClassification;
 import org.api4.java.ai.ml.core.dataset.serialization.DatasetDeserializationFailedException;
 import org.api4.java.ai.ml.core.dataset.splitter.SplitFailedException;
+import org.api4.java.ai.ml.core.dataset.supervised.ILabeledDataset;
+import org.api4.java.ai.ml.core.evaluation.execution.ILearnerRunReport;
+import org.api4.java.ai.ml.core.evaluation.execution.LearnerExecutionFailedException;
+import org.api4.java.algorithm.Timeout;
 import org.api4.java.algorithm.exceptions.AlgorithmException;
 import org.api4.java.algorithm.exceptions.AlgorithmExecutionCanceledException;
 import org.api4.java.algorithm.exceptions.AlgorithmTimeoutedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import weka.classifiers.Classifier;
-import weka.classifiers.meta.FilteredClassifier;
 import weka.core.Instances;
 
+import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Oshando Johnson on 27.09.20
@@ -25,84 +33,97 @@ import java.util.NoSuchElementException;
 public class MLPlanExecutor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MLPlanExecutor.class);
-    FilteredClassifier filteredClassifier;
+    private final int ITERATIONS = 10;
 
     public MLPlanExecutor() {
-        this.filteredClassifier = new FilteredClassifier();
+
     }
 
     /**
-     * Runs ML-Plan with the given instance set.
-     * @param instances training instances
-     * @return hashmap containing F1 measures
+     * Run ML-Plan using the provided path to the ARFF file.
+     *
+     * @param arffFilePath file path for ARFF file
      */
-    public HashMap<String, String> run(Instances instances) {
+    public void evaluateDataset(String arffFilePath) {
 
         long start = System.currentTimeMillis();
 
-        instances.setClassIndex(instances.numAttributes() - 1);
-
-        HashMap<String, String> results = new HashMap<>();
-
-        //Find classifier using ML-Plan
+        //Initialize dataset using ARFF file path
+        ILabeledDataset<?> dataset = null;
         try {
-
-            Classifier classifier = new MLPlanWekaBuilder()
-                    .withDataset(new WekaInstances(instances))
-                    .withPortionOfDataReservedForSelection(.8)
-                    //.withNumCpus(4)
-                    //.withTimeOut(new Timeout(300, TimeUnit.SECONDS))
-                    .withMCCVBasedCandidateEvaluationInSearchPhase(10, .8)
-                    .build()
-                    .call()
-                    .getClassifier();
-
-            long trainTime = (int) (System.currentTimeMillis() - start) / 1000;
-            filteredClassifier.setClassifier(classifier);
-            LOGGER.info("Finished build of the classifier. Training time was {}s.", trainTime);
-
-            LOGGER.info("Chosen model is: {}", (filteredClassifier.getClassifier().getClass().getSimpleName()));
-        } catch (IOException | AlgorithmTimeoutedException | InterruptedException | AlgorithmException | AlgorithmExecutionCanceledException e) {
+            dataset = ArffDatasetAdapter.readDataset(new File(arffFilePath));
+        } catch (DatasetDeserializationFailedException e) {
             e.printStackTrace();
         }
 
-        //Evaluate classifier using MCCV
-        try {
+        //For each iteration, create a new train-test-split and run ML-Plan
+        for (int iteration = 0; iteration < ITERATIONS; iteration++) {
 
-            /* evaluate solution produced by mlplan */
-            ModelEvaluator modelEvaluator = new ModelEvaluator();
-            results = modelEvaluator.monteCarloValidate(instances, filteredClassifier, 0.8, 10);
+            try {
+                List<ILabeledDataset<?>> split = SplitterUtil.getLabelStratifiedTrainTestSplit(dataset, new Random(1337 + (iteration * 11)), 0.7);
+                LOGGER.info("Data read. Time to create dataset object was {}ms", System.currentTimeMillis() - start);
 
-         //Output evaluation results
-            for(String category: results.keySet()){
-                System.out.println("---" + category + "---");
-                System.out.println(filteredClassifier.getClassifier().getClass().getSimpleName()+ ";"+
-                        results.get(category).replace(".", ",").substring(0, results.get(category).lastIndexOf(";")));
+                IWekaClassifier optimizedClassifier = getClassifier(split.get(0));
+
+                /* evaluate solution produced by mlplan */
+                SupervisedLearnerExecutor executor = new SupervisedLearnerExecutor();
+                ILearnerRunReport report = executor.execute(optimizedClassifier, split.get(0), split.get(1));
+
+                System.out.println(report.getPredictionDiffList().getPredictionsAsList().toString());
+
+                LOGGER.info("Error Rate of the solution produced by ML-Plan: {}. ",
+                        EClassificationPerformanceMeasure.F1_WITH_1_POSITIVE.loss(report.getPredictionDiffList().getCastedView(Integer.class, ISingleLabelClassification.class)));
+
+            } catch (SplitFailedException | InterruptedException | LearnerExecutionFailedException e) {
+                e.printStackTrace();
             }
-
-            long totalTime = (int) (System.currentTimeMillis() - start) / 1000;
-            LOGGER.info("ML-Plan execution completed. Total time {}s.", totalTime);
-            return modelEvaluator.getfMeasure();
-        } catch (NoSuchElementException e) {
-            LOGGER.error("Building the classifier failed: {}", LoggerUtil.getExceptionInfo(e));
         }
-        return results;
     }
 
     /**
-     * Returns classifier selected by ML-Plan.
-     * @return WEKA classifier
+     * SReturns trained clssifier
+     * @param trainingSet training set
+     * @return trained classifier
      */
-    public FilteredClassifier getClassifier() {
-        return filteredClassifier;
+    public IWekaClassifier getClassifier(ILabeledDataset<?> trainingSet) {
+
+        IWekaClassifier optimizedClassifier = null;
+        /* initialize mlplan with a tiny search space, and let it run for 30 seconds */
+
+        try {
+            MLPlan<IWekaClassifier> mlPlan = new MLPlanWekaBuilder()
+                    .withNumCpus(8)//Set to about 12 on the server
+                    .withSeed(35467463)
+                    //set default timeout
+                    .withTimeOut(new Timeout(30, TimeUnit.SECONDS))
+                    .withDataset(trainingSet)
+                    .withPortionOfDataReservedForSelection(0.0)//ignore selection phase
+                    .withPerformanceMeasureForSearchPhase(EClassificationPerformanceMeasure.F1_WITH_1_POSITIVE)//use F1
+                    .withMCCVBasedCandidateEvaluationInSearchPhase(10, .7)
+                    .build();
+
+            mlPlan.setLoggerName("testedalgorithm");
+
+            long start = System.currentTimeMillis();
+
+            optimizedClassifier = mlPlan.call();
+
+            long trainTime = (int) (System.currentTimeMillis() - start) / 1000;
+            LOGGER.info("Finished build of the classifier. Training time was {}s.", trainTime);
+            LOGGER.info("Chosen model is: {}", (mlPlan.getSelectedClassifier()));
+            LOGGER.info("Internally believed error was {}", mlPlan.getInternalValidationErrorOfSelectedClassifier());
+
+        } catch (IOException | AlgorithmTimeoutedException | InterruptedException | AlgorithmException | AlgorithmExecutionCanceledException e) {
+            e.printStackTrace();
+        }
+        return optimizedClassifier;
     }
 
-    public static void main(String[] args) throws DatasetDeserializationFailedException, IOException, InterruptedException, SplitFailedException, AlgorithmExecutionCanceledException, AlgorithmTimeoutedException, AlgorithmException {
+    public static void main(String[] args) {
 
-        Instances dataset = Util.loadArffFile("/Users/oshando/Projects/thesis/03-code/swan/swan_core/swan-out/weka/Train_sanitizer_none.arff");
-        //Instances dataset = Util.loadArffFile("/Users/oshando/Projects/thesis/03-code/swan/swan_core/src/main/resources/waveform.arff");
+        String file = "/Users/oshando/Projects/thesis/03-code/swan/swan_core/src/main/resources/waveform.arff";
 
         MLPlanExecutor mlPlan = new MLPlanExecutor();
-        mlPlan.run(dataset);
+        mlPlan.evaluateDataset(file);
     }
 }
