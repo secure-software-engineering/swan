601b36e04a58e581c2e091d4e45e3dd89dca151a
oshando <oshandojohnson@gmail.com>
2022-09-17 21:31:42 +0200

    Refactor module that runs SWAN

--- swan-assist/src/main/java/de/fraunhofer/iem/swan/assist/actions/RunSwanAnalysisImpl.java
+++ swan-assist/src/main/java/de/fraunhofer/iem/swan/assist/actions/RunSwanAnalysisImpl.java
@@ -7,25 +7,11 @@
 
 package de.fraunhofer.iem.swan.assist.actions;
 
-import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.PerformInBackgroundOption;
 import com.intellij.openapi.progress.ProgressManager;
-import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.messages.MessageBus;
-import de.fraunhofer.iem.swan.assist.comm.SwanNotifier;
 import de.fraunhofer.iem.swan.assist.util.Constants;
-import de.fraunhofer.iem.swan.cli.CliRunner;
-import de.fraunhofer.iem.swan.cli.SwanCli;
-import de.fraunhofer.iem.swan.cli.SwanOptions;
-import de.fraunhofer.iem.swan.io.dataset.SrmList;
-import de.fraunhofer.iem.swan.io.dataset.SrmListUtils;
-import org.apache.commons.io.FileUtils;
-import org.jetbrains.annotations.NotNull;
-
 import java.io.File;
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
@@ -36,8 +22,7 @@ public class RunSwanAnalysisImpl {
 
     private static HashMap<String, String> parameters;
     private Project project;
-    private long duration;
-    private SwanCli swan;
+    HashMap<String, String> results;
 
     /**
      * Initializes builder.
@@ -49,6 +34,7 @@ public class RunSwanAnalysisImpl {
 
         this.project = project;
         parameters = param;
+        results = new HashMap<>();
     }
 
     /**
@@ -63,106 +49,7 @@ public class RunSwanAnalysisImpl {
         if (!outputFolder.exists())
             outputFolder.mkdir();
 
-        /*File logFile = new File(outputFolder, currentTimestamp + parameters.get(Constants.OUTPUT_LOG));
-        try {
-            logFile.createNewFile();
-            parameters.replace(Constants.OUTPUT_LOG, logFile.getPath());
-
-            FileOutputStream fileOutputStream = new FileOutputStream(logFile.getAbsolutePath());
-
-            System.setOut(new PrintStream(fileOutputStream));
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
-
-        ProgressManager.getInstance().run(new Task.Backgroundable(project, resource.getString("Messages.Title.Progress")) {
-            @Override
-            public void run(@NotNull ProgressIndicator indicator) {
-
-                long start = System.currentTimeMillis();
-
-                SwanOptions options = new CliRunner().initializeOptions();
-                options.setTestDataDir(parameters.get(Constants.SOURCE_DIRECTORY));
-                options.setOutputDir( parameters.get(Constants.OUTPUT_DIRECTORY));
-                options.setToolkit(parameters.get(Constants.TOOLKIT).toLowerCase());
-                options.setPhase("predict");
-                options.setTrainDataDir("");
-
-
-                swan = new SwanCli();
-                try {
-                    swan.run(options);
-                } catch (IOException | InterruptedException e) {
-                    throw new RuntimeException(e);
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
-                }
-                duration = System.currentTimeMillis() - start;
-            }
-
-            @Override
-            public void onCancel() {
-                super.onCancel();
-            }
-
-            @Override
-            public void onSuccess() {
-                super.onSuccess();
-
-
-                SrmList srmList = swan.getSwanPipeline().getModelEvaluator().getPredictedSrmList();
-
-                String filename = parameters.get(Constants.OUTPUT_DIRECTORY) + File.separator + "srm-" + getCurrentTimestamp() + ".json";
-
-                try {
-                    SrmListUtils.exportFile(srmList, filename);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-
-
-                //Create copy of file
-                /*copyFile(new File(parameters.get(Constants.OUTPUT_FILE)));
-
-                JSONFileParser parser = new JSONFileParser(parameters.get(Constants.OUTPUT_FILE));
-                HashMap<String, MethodWrapper> exportedMethods = parser.parseJSONFileMap();*/
-
-                HashMap<String, String> results = new HashMap<String, String>();
-                results.put(Constants.OUTPUT_FILE, filename);
-                results.put(Constants.OUTPUT_LOG, "");
-
-                int m = (int) (((duration / 1000) / 60) % 60);
-                int s = (int) ((duration / 1000) % 60);
-
-                results.put(Constants.ANALYSIS_RESULT, srmList.getMethods().size() + " methods found in " + m + " mins " + s + " secs");
-
-                MessageBus messageBus = project.getMessageBus();
-                SwanNotifier publisher = messageBus.syncPublisher(SwanNotifier.END_SWAN_PROCESS_TOPIC);
-                publisher.launchSwan(results);
-            }
-        });
-    }
-
-    private void copyFile(File original) {
-
-        File copied = new File(
-                parameters.get(Constants.OUTPUT_DIRECTORY) + File.separator + getCurrentTimestamp() + "-config.json");
-        try {
-            FileUtils.copyFile(original, copied);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * Get the timestamp in a specified format.
-     *
-     * @return Formatted date
-     */
-    private String getCurrentTimestamp() {
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd-HHmmss");
-        return LocalDateTime.now().format(formatter);
+        ProgressManager.getInstance().run(new RunSwanTask(project, "Detecting SRMs", true,
+                PerformInBackgroundOption.ALWAYS_BACKGROUND, parameters));
     }
 }
