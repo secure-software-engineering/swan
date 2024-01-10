b3d349dea75d16b31e9829a2bfb97518357786d8
ssegit <srk240998@gmail.com>
2024-01-10 16:20:43 +0100

    Set identifier for training methods

--- swan-assist/src/main/java/de/fraunhofer/iem/swan/assist/ui/MethodListTree.java
+++ swan-assist/src/main/java/de/fraunhofer/iem/swan/assist/ui/MethodListTree.java
@@ -587,10 +587,12 @@ public class MethodListTree extends Tree {
 
                     classNode.add(addCategoriesToNode(method));
 
-                    if (method.getMethod().isKnown())
+                    if (method.getMethod().isKnown()) {
+                        method.setTrainingMethod(true);
                         standardSrms.add(classNode);
-                    else
+                    }else{
                         currentProject.add(classNode);
+                    }
                 }
             }
 
